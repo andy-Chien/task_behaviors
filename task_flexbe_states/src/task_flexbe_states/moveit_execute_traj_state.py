5e6cc4261790a1d14f53f33441cbb7098a9b4179
Andy Chien <r960411@gmail.com>
2022-03-27 16:14:25 +0800

    fix error in execute traj

--- task_flexbe_states/src/task_flexbe_states/moveit_execute_traj_state.py
+++ task_flexbe_states/src/task_flexbe_states/moveit_execute_traj_state.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 import rospy
-import threading
 import moveit_commander
-import actionlib
-from moveit_msgs.msg import MoveItErrorCodes, ExecuteTrajectoryAction, RobotTrajectory, ExecuteTrajectoryActionGoal
+from moveit_msgs.msg import MoveItErrorCodes, ExecuteTrajectoryAction, RobotTrajectory, ExecuteTrajectoryGoal
 from flexbe_core.proxy.proxy_action_client import ProxyActionClient
 from flexbe_core import EventState
 
@@ -36,10 +34,9 @@ class MoveItExecuteTrajectoryState(EventState):
 											input_keys=['joint_trajectory', 'block_execute'])
 		self._group_name = robot_name
 		self._move_group = moveit_commander.MoveGroupCommander(self._group_name)
-		self._result = MoveItErrorCodes.FAILURE
 		self._exe_topic = '/execute_trajectory'
 		self._exe_client = ProxyActionClient({self._exe_topic: ExecuteTrajectoryAction})
-		self._thread = None
+		self._running = False
 
 	def stop(self):
 		pass
@@ -48,39 +45,28 @@ class MoveItExecuteTrajectoryState(EventState):
 		'''
 		Execute this state
 		'''
-		# if self._thread is not None and self._thread.is_alive():
-		# 	return 'running'
-
 		if self._exe_client.has_result(self._exe_topic):
 			result = self._exe_client.get_result(self._exe_topic)
-			
-			if result.error_code == MoveItErrorCodes.SUCCESS or result.error_code == MoveItErrorCodes.PREEMPTED:
-				self._thread = None
+			error_code = result.error_code.val
+			if error_code == MoveItErrorCodes.SUCCESS or error_code == MoveItErrorCodes.PREEMPTED:
+				self._running = False
 				return 'done'
-			elif result.error_code == MoveItErrorCodes.MOTION_PLAN_INVALIDATED_BY_ENVIRONMENT_CHANGE:
-				self._thread = None
-				rospy.logwarn('[MoveIt Execute Trajectory State]: ' + str(result.error_code))
+			elif error_code == MoveItErrorCodes.MOTION_PLAN_INVALIDATED_BY_ENVIRONMENT_CHANGE:
+				rospy.logwarn('[MoveIt Execute Trajectory State]: ' + str(error_code))
+				self._running = False
 				return 'collision'
 			else:
-				self._thread = None
-				rospy.logerr('[MoveIt Execute Trajectory State]: MoveItErrorCodes = {}, typt = {}'.format(self._result, type(self._result)))
+				rospy.logerr('[MoveIt Execute Trajectory State]: MoveItErrorCodes = {}'.format(error_code))
+				self._running = False
 				return 'failed'
 		else:
+			self._running = True
 			return 'running'
 
 	def on_enter(self, userdata):
-		goal = ExecuteTrajectoryActionGoal(userdata.joint_trajectory)
-		self._exe_client.send_goal(self._exe_topic, goal)
-
-		# if userdata.block_execute:
-		# 	print('1')
-		# 	self._result = self._move_group.execute(userdata.joint_trajectory)
-		# else:
-		# 	if self._thread is None:
-		# 		print('2')
-		# 		self._thread = threading.Thread(target = self.execute_trajectory, 
-		# 										args=(userdata.joint_trajectory,))
-		# 		self._thread.start()
+		if not self._running:
+			goal = ExecuteTrajectoryGoal(userdata.joint_trajectory)
+			self._exe_client.send_goal(self._exe_topic, goal)
 
 	def on_stop(self):
 		pass
@@ -90,6 +76,3 @@ class MoveItExecuteTrajectoryState(EventState):
 
 	def on_resume(self, userdata):
 		self.on_enter(userdata)
-
-	def execute_trajectory(self, jt):
-		self._result = self._move_group.execute(jt)
