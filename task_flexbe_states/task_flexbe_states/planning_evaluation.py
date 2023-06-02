53164217e143c1e924d2bfb47a8b7a9999729e89
Andy Chien <808790017@gms.tku.edu.tw>
2023-06-02 11:27:09 +0800

    update for evaluation

--- task_flexbe_states/task_flexbe_states/planning_evaluation.py
+++ task_flexbe_states/task_flexbe_states/planning_evaluation.py
@@ -42,6 +42,8 @@ class PlanningEvaluation(EventState):
         self.planner = planner
         self.namespace = namespace
         self.not_save_yet = True
+        self.success_buffer = []
+        self.r100_success_record = []
 
         if len(namespace) > 1 or (len(namespace) == 1 and namespace.startswith('/')):
             namespace = namespace[1:] if namespace[0] == '/' else namespace
@@ -61,6 +63,7 @@ class PlanningEvaluation(EventState):
             result = self._traj_length_client.result(self._compute_traj_service)
 
             self.success_count += 1
+            self.success_buffer.append(1.0)
             self.total_planning_success_time += userdata.planning_time
             self.total_joint_trajectory_length += result.joint_traj_length
             self.total_tool_trajectory_length += result.tool_traj_length
@@ -79,32 +82,49 @@ class PlanningEvaluation(EventState):
             pec == MoveItErrorCodes.TIMED_OUT:
             if pec == MoveItErrorCodes.SUCCESS or not self.last_plan_failed:
                 self.planning_count += 1
+                if len(self.success_buffer) < self.planning_count:
+                    self.success_buffer.append(0.0)
             self.last_plan_failed = pec != MoveItErrorCodes.SUCCESS
         else:
             return 'done'
 
         if self.do_evaluation and self.planning_count > self.warm_up_cnt and self.planning_count > 0:
             self.success_rate = self.success_count / self.planning_count
+            if 0 < len(self.success_buffer) < 100:
+                r100_success_rate = sum(self.success_buffer) / len(self.success_buffer)
+            else:
+                r100_success_rate = sum(self.success_buffer[-100 :]) / 100.0
+            if self.planning_count % 100 < 1:
+                self.r100_success_record.append(r100_success_rate)
             self._logger.info('===================================================================')
-            self._logger.info('cnt: {}, s_rate: {}, p_time: {}, jt_len: {}, tt_len: {}'.format(
-                self.planning_count, self.success_rate, self.avg_planning_success_time, \
+            self._logger.info('cnt: {}, s_rate: {}, r100_s_r: {}, p_time: {}, jt_len: {}, tt_len: {}'.format(
+                self.planning_count, self.success_rate, r100_success_rate, self.avg_planning_success_time, \
                 self.avg_joint_trajectory_length, self.avg_tool_trajectory_length))
             self._logger.info('===================================================================')
 
         if self.warm_up_cnt > 0 and self.planning_count >= self.warm_up_cnt:
             self.warm_up_cnt = -1
             self.planning_count = 0.0
+            self.success_buffer = []
+            self.r100_success_record = []
 
         if self.planning_count >= self.eval_rounds and self.not_save_yet:
             self.not_save_yet = False
-            with open(PATH + self.planner + '_' + self.namespace + '.txt', 'w') as f:
-                f.writelines('success_rate: ' + str(self.success_rate) +'\n')
-                f.writelines('planning_time: ' + str(self.avg_planning_success_time) +'\n')
+            name = self.planner + '_' + self.namespace
+            with open(PATH + name + '.txt', 'w') as f:
+                f.writelines(name +'_success_rate = ' + str(self.success_rate) +'\n')
+                f.writelines(name +'_success_data = [')
+                f.writelines([str(x) + ', ' for x in self.r100_success_record])
+                f.writelines(']\n\n' + name + '_planning_time = ' + str(self.avg_planning_success_time) +'\n')
+                f.writelines(name +'_planning_time_data = [')
                 f.writelines([str(x) + ', ' for x in self.planning_time])
-                f.writelines('\n\njoint_trajectory_length: ' + str(self.avg_joint_trajectory_length) +'\n')
+                f.writelines(']\n\n' + name + '_joint_trajectory_length: ' + str(self.avg_joint_trajectory_length) +'\n')
+                f.writelines(name +'_joint_trajectory_length_data = [')
                 f.writelines([str(x) + ', ' for x in self.joint_trajectory_length])
-                f.writelines('\n\ntool_trajectory_length: ' + str(self.avg_tool_trajectory_length) +'\n')
+                f.writelines(']\n\n' + name + '_tool_trajectory_length: ' + str(self.avg_tool_trajectory_length) +'\n')
+                f.writelines(name +'_tool_trajectory_length_data = [')
                 f.writelines([str(x) + ', ' for x in self.tool_trajectory_length])
+                f.writelines(']')
                 f.close()
 
         return 'finish' if self.terminal and self.planning_count >= self.terminal else 'done'
