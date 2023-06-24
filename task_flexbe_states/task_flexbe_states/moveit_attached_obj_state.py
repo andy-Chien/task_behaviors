cf0e8a4c98a92946e63535ae496d69eb1072721f
Andy Chien <r960411@gmail.com>
2023-06-25 04:40:54 +0800

    test attached state

--- task_flexbe_states/task_flexbe_states/moveit_attached_obj_state.py
+++ task_flexbe_states/task_flexbe_states/moveit_attached_obj_state.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
 import rclpy
-import open3d as o3d
-from moveit_msgs.msg import MoveItErrorCodes, AttachedCollisionObject
-from shape_msgs.msg import SolidPrimitive, Mesh
-from geometry_msgs.msg import Pose
+import numpy as np
+from moveit_msgs.msg import AttachedCollisionObject, CollisionObject
+from shape_msgs.msg import SolidPrimitive, Mesh, MeshTriangle
+from geometry_msgs.msg import Pose, Point
 from flexbe_core.proxy.proxy_publisher import ProxyPublisher
 from flexbe_core import EventState
+import open3d as o3d
 
 '''
 Created on 24.02.2022
@@ -29,30 +29,42 @@ class MoveItAttachedObjState(EventState):
     '''
 
 
-    def __init__(self, mesh_file='', operation='add', obj_type='', link_name='', \
-                 touch_links=[], size=None, namespace='', obj_name='', pos=[0,0,0], quat=[1,0,0,0]):
+    def __init__(self, mesh_file='', operation='add', obj_type='', link_name='',
+                 touch_links=[], size=None, namespace='', obj_name='', pos=[0.0,0.0,0.0], quat=[1.0,0.0,0.0,0.0]):
         '''
         Constructor
         '''
         super(MoveItAttachedObjState, self).__init__(outcomes=['done'])
-        if len(namespace) > 0 and not namespace.startswith('/'): 
+        if len(namespace) > 0 and not namespace.startswith('/'):
             namespace = '/' + namespace 
-
-        self._attach_topic = namespace + '/attached_collision_object'
+        
         self.obj_name = obj_name
         self.mesh_file = mesh_file
         self.operation = operation
-        self.type = obj_type
-        self.link_name = link_name
-        self.touch_links = touch_links
+        self.obj_type = obj_type
         self.size = size
         self.pos = pos
         self.quat = quat
+
+        if len(namespace) > 1 or (len(namespace) == 1 and namespace.startswith('/')):
+            namespace = namespace[1:] if namespace[0] == '/' else namespace
+            self._attach_topic = '/' + namespace + '/attached_collision_object'
+            self._obj_topic = '/' + namespace + '/collision_object'
+            self.touch_links = [namespace + '_' + jn for jn in touch_links]
+            self.link_name = namespace + '_' + link_name
+        else:
+            self._attach_topic = '/attached_collision_object'
+            self._obj_topic = '/collision_object'
+            self.link_name = link_name
+            self.touch_links = touch_links
+            
+
 
         ProxyPublisher._initialize(EventState._node)
         self._logger = self._node.get_logger()
 
         self._publisher = ProxyPublisher({self._attach_topic: AttachedCollisionObject})
+        self._obj_publisher = ProxyPublisher({self._obj_topic: CollisionObject})
 
     def stop(self):
         pass
@@ -61,25 +73,39 @@ class MoveItAttachedObjState(EventState):
         '''
         Execute this state
         '''
+        self._logger.info('touch_links = {}'.format(self.touch_links))
+        self._logger.info('mesh_file = {}'.format(self.mesh_file))
+        pose = Pose()
+        pose.position.x = self.pos[0]
+        pose.position.x = self.pos[1]
+        pose.position.x = self.pos[2]
+        pose.orientation.w = self.quat[0]
+        pose.orientation.x = self.quat[1]
+        pose.orientation.y = self.quat[2]
+        pose.orientation.z = self.quat[3]
         msg = AttachedCollisionObject()
         msg.link_name = self.link_name
         msg.touch_links = self.touch_links
         msg.object.id = self.obj_name
-        msg.object.header
-        msg.object.pose
-        msg.object.operation = self.operation
-        p = Pose()
-        p.position.x = self.pos[0]
-        p.position.x = self.pos[1]
-        p.position.x = self.pos[2]
-        p.orientation.w = self.quat[0]
-        p.orientation.x = self.quat[1]
-        p.orientation.y = self.quat[2]
-        p.orientation.z = self.quat[3]
+        msg.object.header.frame_id = self.link_name
+        msg.object.pose = pose
+        msg.object.operation = CollisionObject.ADD if 'add' in self.operation else CollisionObject.REMOVE
 
         if 'mesh' in self.obj_type and self.mesh_file != '':
-            msg.object.meshes = 
-            msg.object.mesh_poses.append(p)
+            mesh_msg = Mesh()
+            o3d_mesh = o3d.io.read_triangle_mesh(self.mesh_file)
+            vertices = np.asarray(o3d_mesh.vertices)
+            triangles = np.asarray(o3d_mesh.triangles)
+            for v in vertices:
+                p = Point()
+                p.x, p.y, p.z = v
+                mesh_msg.vertices.append(p)
+            for t in triangles:
+                mt = MeshTriangle()
+                mt.vertex_indices = np.array(t, dtype=np.uint32)
+                mesh_msg.triangles.append(mt)
+            msg.object.meshes.append(mesh_msg)
+            msg.object.mesh_poses.append(Pose())
         else: 
             sp = SolidPrimitive()
             sp.dimensions = self.size
@@ -96,7 +122,11 @@ class MoveItAttachedObjState(EventState):
                 ok = False
             if ok:
                 msg.object.primitives.append(sp)
-                msg.object.primitive_poses.append(p)
+                msg.object.primitive_poses.append(Pose())
+        self._publisher.publish(self._attach_topic, msg)
+        if 'remove' in self.operation:
+            self._obj_publisher.publish(self._obj_topic, msg.object)
+
         return 'done'
 
     def on_enter(self, userdata):
