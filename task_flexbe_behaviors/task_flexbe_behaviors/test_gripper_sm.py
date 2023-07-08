70fb229417aa7f5b57a709651e9f1c575a4ba79f
Andy Chien <r960411@gmail.com>
2023-07-09 03:11:37 +0800

    modify gripper param

--- task_flexbe_behaviors/task_flexbe_behaviors/test_gripper_sm.py
+++ task_flexbe_behaviors/task_flexbe_behaviors/test_gripper_sm.py
@@ -20,68 +20,69 @@ Created on Mon Jun 12 2023
 @author: TaiTing Tsai
 '''
 class TestGripperSM(Behavior):
-	'''
-	test hiwin xeg32 gripper api
-	'''
+    '''
+    test hiwin xeg32 gripper api
+    '''
 
 
-	def __init__(self, node):
-		super(TestGripperSM, self).__init__()
-		self.name = 'Test Gripper'
+    def __init__(self, node):
+        super(TestGripperSM, self).__init__()
+        self.name = 'Test Gripper'
 
-		# parameters of this behavior
+        # parameters of this behavior
+        self.add_parameter('namespace', '')
 
-		# references to used behaviors
-		OperatableStateMachine.initialize_ros(node)
-		ConcurrencyContainer.initialize_ros(node)
-		PriorityContainer.initialize_ros(node)
-		Logger.initialize(node)
-		HiwinXeg32GripperApi.initialize_ros(node)
+        # references to used behaviors
+        OperatableStateMachine.initialize_ros(node)
+        ConcurrencyContainer.initialize_ros(node)
+        PriorityContainer.initialize_ros(node)
+        Logger.initialize(node)
+        HiwinXeg32GripperApi.initialize_ros(node)
 
-		# Additional initialization code can be added inside the following tags
-		# [MANUAL_INIT]
+        # Additional initialization code can be added inside the following tags
+        # [MANUAL_INIT]
 		
 		# [/MANUAL_INIT]
 
-		# Behavior comments:
+        # Behavior comments:
 
 
 
-	def create(self):
-		# x:879 y:371, x:457 y:119
-		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['gripper_mode'])
-		_state_machine.userdata.direction = 1
-		_state_machine.userdata.distance = 10
-		_state_machine.userdata.speed = 500
-		_state_machine.userdata.holding_stroke = 10
-		_state_machine.userdata.holding_speed = 500
-		_state_machine.userdata.holding_force = 80
-		_state_machine.userdata.flag = 1
-		_state_machine.userdata.close = 'close'
-		_state_machine.userdata.open = 'open'
-		_state_machine.userdata.expert = 'expert'
-		_state_machine.userdata.off = 'off'
-		_state_machine.userdata.gripper_mode = 'close'
+    def create(self):
+        # x:879 y:371, x:457 y:119
+        _state_machine = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['gripper_mode'])
+        _state_machine.userdata.direction = 0
+        _state_machine.userdata.distance = 0
+        _state_machine.userdata.speed = 6000
+        _state_machine.userdata.holding_stroke = 3200
+        _state_machine.userdata.holding_speed = 2000
+        _state_machine.userdata.holding_force = 40
+        _state_machine.userdata.flag = 1
+        _state_machine.userdata.close = 'close'
+        _state_machine.userdata.open = 'open'
+        _state_machine.userdata.expert = 'expert'
+        _state_machine.userdata.off = 'off'
+        _state_machine.userdata.gripper_mode = 'close'
 
-		# Additional creation code can be added inside the following tags
-		# [MANUAL_CREATE]
+        # Additional creation code can be added inside the following tags
+        # [MANUAL_CREATE]
 		
 		# [/MANUAL_CREATE]
 
 
-		with _state_machine:
-			# x:376 y:293
-			OperatableStateMachine.add('hiwin2',
-										HiwinXeg32GripperApi(),
-										transitions={'done': 'finished', 'failed': 'failed'},
-										autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off},
-										remapping={'mode': 'gripper_mode', 'direction': 'direction', 'distance': 'distance', 'speed': 'speed', 'holding_stroke': 'holding_stroke', 'holding_speed': 'holding_speed', 'holding_force': 'holding_force', 'flag': 'flag'})
+        with _state_machine:
+            # x:376 y:293
+            OperatableStateMachine.add('hiwin2',
+                                        HiwinXeg32GripperApi(sim=False, namespace=self.namespace),
+                                        transitions={'done': 'finished', 'failed': 'failed'},
+                                        autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off},
+                                        remapping={'mode': 'gripper_mode', 'direction': 'direction', 'distance': 'distance', 'speed': 'speed', 'holding_stroke': 'holding_stroke', 'holding_speed': 'holding_speed', 'holding_force': 'holding_force', 'flag': 'flag'})
 
 
-		return _state_machine
+        return _state_machine
 
 
-	# Private functions can be added inside the following tags
-	# [MANUAL_FUNC]
+    # Private functions can be added inside the following tags
+    # [MANUAL_FUNC]
 	
 	# [/MANUAL_FUNC]
